27506b15333fa11ca0e09a7c269830d496627c74
huangjianjun <huangjianjun@cvte.com>
2023-06-06 10:32:08 +0800

    [improve] format code

--- app/src/main/java/xyz/juncat/scrollerimprove/SubFragment.kt
+++ app/src/main/java/xyz/juncat/scrollerimprove/SubFragment.kt
@@ -14,49 +14,49 @@ import androidx.recyclerview.widget.RecyclerView
 
 class SubFragment : Fragment() {
 
-        override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            return FrameLayout(requireContext()).apply {
-                addView(CombineWithViewPager2RecyclerView(requireContext()).also {
-                    it.adapter = object : RecyclerView.Adapter<ViewHolder>() {
-                        override fun onCreateViewHolder(
-                            parent: ViewGroup,
-                            viewType: Int
-                        ): ViewHolder {
-                            return ViewHolder(TextView(context).apply {
-                                layoutParams =
-                                    ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 112)
-                                setTextColor(Color.BLACK)
-                                gravity = Gravity.CENTER
-                            })
-                        }
-
-                        override fun getItemCount(): Int {
-                            return 1000
-                        }
-
-                        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-                            (holder.itemView as? TextView)?.text = position.toString()
-                        }
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FrameLayout(requireContext()).apply {
+            addView(CombineWithViewPager2RecyclerView(requireContext()).also {
+                it.adapter = object : RecyclerView.Adapter<ViewHolder>() {
+                    override fun onCreateViewHolder(
+                        parent: ViewGroup,
+                        viewType: Int
+                    ): ViewHolder {
+                        return ViewHolder(TextView(context).apply {
+                            layoutParams =
+                                ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 112)
+                            setTextColor(Color.BLACK)
+                            gravity = Gravity.CENTER
+                        })
                     }
-                    it.layoutManager = LinearLayoutManager(requireContext())
-                }, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-            }
-        }
 
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-        }
+                    override fun getItemCount(): Int {
+                        return 1000
+                    }
 
-        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+                    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+                        (holder.itemView as? TextView)?.text = position.toString()
+                    }
 
+                }
+                it.layoutManager = LinearLayoutManager(requireContext())
+            }, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
         }
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
